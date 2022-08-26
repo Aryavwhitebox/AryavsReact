c4f16fdfb627f31fb52c6be7b0fa989bd926a291
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-08-26 09:25:45 -0700

    Update ExpenseItem.js

--- 1ExpenseItemComponent/src/component/ExpenseItem.js
+++ 1ExpenseItemComponent/src/component/ExpenseItem.js
@@ -1,10 +1,10 @@
 import './ExpenseItem.css'
 function ExpenseItem(){
-    let title="car insurance"
+    
 return(
     <div className='expense-item'>
         <div>24th August</div>
-    <div className='expense-item_description'><h2>{title}</h2></div>
+    <div className='expense-item_description'><h2>car insurance</h2></div>
     <div className='expense-item_price'>200$</div>
     </div>
 );
