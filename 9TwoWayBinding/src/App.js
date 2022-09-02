6c5cbcf4b2c7ec1d5cfca3a04f7b88e9c68ac8b9
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-09-02 08:43:12 -0700

    Add files via upload

---
+++ 9TwoWayBinding/src/App.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses'
+const App = () => {
+  let expenses=[
+    {
+      id:'e1',
+      title:'table',
+      amount:250,
+      date: new Date(2021,5,16) 
+    },
+    { 
+      id:'e2',
+      title:'chair',
+      amount:230,
+      date: new Date(2020,2,15)
+    },
+    {
+      id:'e3',
+      title:'tea poy',
+      amount:290,
+      date: new Date(2021,4,12)
+    } ,
+    {
+      id:'e4',
+      title:'rack',
+      amount:350,
+      date: new Date(2020,9,11)
+    }
+  ];
+  return (
+   <div>
+    
+     <NewExpense/>
+     <Expenses item={expenses}></Expenses>
+   </div>
+  );
+}
+
+export default App;
